dc5f13caca5c6ddeaad7e575ea83c4f5c4d1d621
GAURAB <gauravthapa853@gmail.com>
2024-02-24 13:30:36 +0530

    Added Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import Navbar from "@/components/navbar/page";
 import AlumiPage from "@/components/screens/alumni-page/page";
+import Heropage from "@/components/screens/hero-page/page";
 import LeapAdvantagePage from "@/components/screens/leap-advantage-page/page";
 import StudentAckPage from "@/components/screens/student-ack-page/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+        <Navbar/>
+        <Heropage/>
         <LeapAdvantagePage/>
         <StudentAckPage/>
         <AlumiPage/>
---
+++ src/components/navbar/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div className="navbar  shadow-2xl bg-base-100 ">
+        <div className="navbar-start">    
+            <a className="btn btn-ghost text-xl text-[#4d30ff] ml-28 ">Leap Advantages</a>
+        </div>
+        <div className="navbar-end">
+            <a className="btn mr-28  text-white text-xl bg-primary ">For Masters</a>
+        </div>
+        </div>
+  )
+}
+
+export default Navbar
